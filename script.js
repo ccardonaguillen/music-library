96cc587d716428cec29d2b5f38e8cca629a6c273
ccardona <ccardonaguillen@gmail.com>
2022-09-11 16:05:06 +0200

    Create skeleton files and Record constructor

---
+++ script.js
@@ -0,0 +1,7 @@
+function Record(title, artist, release_year, owned, format="") {
+    this.title = title;
+    this.artist = artist;
+    this.release_year = release_year;
+    this.format = format;
+    this.owned = owned
+}
