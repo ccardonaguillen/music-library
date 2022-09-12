22a4217cf7d24ae6beff8c6096970cf6ba701e00
ccardona <ccardonaguillen@gmail.com>
2022-09-12 17:35:02 +0200

    Added logic to add and remove entries from library and table

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-let musicLibrary = [];
-let albumID = 0;
-
 function Album({title, artist, release_year, owned, format=[]}) {
     this.title = title;
     this.artist = artist;
     this.release_year = release_year;
-    this.owned = owned;
+    this.owned = Boolean(owned)
     this.format = format.join(", ");
+    this.id = title.toLowerCase().replaceAll(/\W/g, "")
+            + artist.toLowerCase().replaceAll(/\W/g, "");
 }
 
 class MusicLibrary {
@@ -14,38 +13,97 @@ class MusicLibrary {
         this.albumList = []
     }
 
-    addAlbum(album) {
-        this.albumList.push(album);
+    addAlbum(newAlbum) {
+        if (this.albumList.every(album =>
+                                 newAlbum.id !== album.id)) {
+            this.albumList.push(newAlbum);
+        } else {
+            alert('This album already exists')
+        }
     }
 
-    displayLibrary() {
-        clearLibrary()
-        this.albumList.forEach(album => {displayAlbum(album)})
+    deleteAlbum(id) {
+        this.albumList = this.albumList.filter(album => album.id !== id);
     }
 }
 
-function clearLibrary() {
+
+function displayLibrary() {
+    library.albumList.forEach(album => {displayNewEntry(album)})
+}
+
+function clearDisplay() {
     const libraryTable = document.querySelector('table > tbody');
     while (libraryTable.lastElementChild) {
         libraryTable.lastElementChild.remove();
     }
 }
 
-function displayAlbum(album) {
-    const libraryTable = document.querySelector('table > tbody');
+function updateDisplay() {
+    clearDisplay();
+    displayLibrary();
+}
 
-    const tableRow = document.createElement('tr')
-    tableRow.setAttribute("data_id", albumID);
-    albumID ++
+function displayNewEntry(album) {
+    const tableRow = document.createElement('tr');
 
-    for (let prop in album) {
+    tableRow.setAttribute('data-id', album.id);
+
+    // Add album info
+    tableColumns = ['title', 'artist', 'release_year', 'owned', 'format'];
+    for (const prop of tableColumns) {
         const dataCell = document.createElement('td');
-        dataCell.textContent = album[prop];
+        
+        if (prop === 'owned') {
+            dataCell.textContent = album[prop] ?
+                                   'Yes' :
+                                   'No';
+        } else {
+            dataCell.textContent = album[prop];
+        }
 
         tableRow.appendChild(dataCell);
     }
 
-    libraryTable.appendChild(tableRow);
+    // Create remove-album button
+    addRemoveButton(tableRow);
+
+    // Append new row
+    tableContents.appendChild(tableRow);
+}
+
+function removeEntry(e) {
+    if (!confirm("Are you sure you want to delete this album?")) return;
+
+    const entryRow = e.target.parentElement.parentElement;
+    const id = entryRow.getAttribute("data-id");
+
+    library.deleteAlbum(id)
+
+    updateDisplay()
+}
+
+function addRemoveButton(row) {
+    const dataCell = document.createElement('td');
+    const removeButton = document.createElement('button');
+
+    removeButton.classList.add('remove-album', 'img-button', 'hidden');
+    
+
+    dataCell.appendChild(removeButton);
+    row.appendChild(dataCell);
+
+    // Connect new row so that remove-icon only appears on hover
+    row.addEventListener('mouseenter', function() {
+        removeButton.classList.remove('hidden');
+    })
+
+    row.addEventListener('mouseleave', function() {
+        removeButton.classList.add('hidden');
+    })
+
+    // Connect button to removeAlbum function
+    removeButton.addEventListener('click', removeEntry)
 }
 
 function openModal() {
@@ -53,40 +111,62 @@ function openModal() {
     document.getElementById('new-title').focus()
 }
 
-function closeModal() {modal.classList.add('hidden')}
+function closeModal() {
+    modal.classList.add('hidden');
+    newAlbumForm.reset();
+}
 
+function disableCheckBoxes() {
+    formatCheckBoxes.forEach(checkBox => {
+        checkBox.disabled = true;
+    })
+}
+
+function enableCheckBoxes() {
+    formatCheckBoxes.forEach(checkBox => {
+        checkBox.disabled = false;
+    })
+}
 
 function submitNewAlbum(e) {
     e.preventDefault();
 
-    const form = e.target;
-    const newAlbum = new Album(processForm(form));
-
+    const newAlbum = new Album(processNewAlbumForm());
     library.addAlbum(newAlbum);
-    library.displayLibrary();
 
+    updateDisplay();
     closeModal();
-    form.reset();
 }
 
-function processForm(form) {
-    let formData = new FormData(form);
+function processNewAlbumForm() {
+    let formData = new FormData(newAlbumForm);
 
     formContent = Object.fromEntries(formData.entries());
-    formContent['format'] = formData.getAll("format");
+    formContent['owned'] = formContent['owned'] === 'true' ?
+                           true :
+                           false;
+    formContent['format'] = formData.getAll('format');
 
     return formContent;
 }
 
+let library = new MusicLibrary();
 
+const tableContents = document.querySelector('table > tbody');
 const modal = document.querySelector('.modal-overlay');
 const openModalButton = document.getElementById('open-modal');
+const closeModalButton = document.getElementById('close-modal');
 const newAlbumForm = document.getElementById('add-album');
+const ownsTrueButton = document.getElementById('owns-true');
+const ownsFalseButton = document.getElementById('owns-false');
+const formatCheckBoxes = document.getElementsByName('format');
+
 
 openModalButton.addEventListener('click', openModal);
+closeModalButton.addEventListener('click', closeModal)
 newAlbumForm.addEventListener('submit', submitNewAlbum);
-
-library = new MusicLibrary();
+ownsTrueButton.addEventListener('click', enableCheckBoxes);
+ownsFalseButton.addEventListener('click', disableCheckBoxes);
 
 album1 = new Album({title: "Lonerism",
                     artist: "Tame Impala",
@@ -94,7 +174,7 @@ album1 = new Album({title: "Lonerism",
                     owned: false
                 })
 
-album2 = new Album({title: "Favourite Worse Nightmare",
+album2 = new Album({title: "Favourite Worst Nightmare",
                     artist: "Arctic Monkeys",
                     release_year: 2007,
                     owned: true,
@@ -112,4 +192,4 @@ library.addAlbum(album2);
 library.addAlbum(album3);
 
 
-library.displayLibrary()
+updateDisplay()
