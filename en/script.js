a1b9f2fcfe95c32a2a8b9c7f9fc5cc3b25d34d69
ccardona <ccardonaguillen@gmail.com>
2022-09-23 11:50:00 +0200

    Create prototype album loader

--- en/script.js
+++ en/script.js
@@ -1,3 +1,27 @@
+const fileLoader = document.getElementById('file-loader');
+
+fileLoader.addEventListener('change', (event) => {
+    const fileList = event.target.files;
+    var file = fileList[0];
+    readFile(file);
+    fileLoader.classList.add("hidden");
+  });
+
+function readFile(f) {
+    const reader = new FileReader();
+    var fileContent
+    reader.addEventListener('load', (event) => {
+        var fileContent = JSON.parse(event.target.result);
+        Object.values(fileContent).forEach(val => {
+            var album = new Album(val);
+            library.addAlbum(album);
+        })
+        updateDisplay();
+    });
+    reader.readAsText(f);
+}
+
+
 /* INITIALISE FILTER AND SORTING OBJECTS*/
 let currFilter = { type: "", value: "" };
 let currSorting = { by: "title", ord: "asc" };
